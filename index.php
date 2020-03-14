f11fe0a3d9f8fd92eed92ad9c79cdba040c48069
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-03-14 19:25:43 +0100

    init

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mundanuem - Web de connaissane</title>
+
+    <link rel="stylesheet" href="./assets/main.css">
+</head>
+
+<body>
+
+    <header class="header-page">
+
+        <div class="wrapper-page">
+            
+        </div>
+        
+    </header>
+
+    <div class="wrapper-page">
+
+        <main class="main-page">
+
+        </main>
+
+        <aside class="aside-page">
+            
+        </aside>
+
+    </div>
+
+</body>
+
+</html>
