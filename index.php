3fc2f7caf622e19c83b84ec95154496a5cc02c2a
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-03-15 15:09:34 +0100

    essaie curseur

--- index.php
+++ index.php
@@ -11,71 +11,97 @@
 
 <body>
 
-<div class="bg-page"></div>
+    <div class="bg-page"></div>
 
-<div class="wrapper-general">
+    <div class="wrapper-general">
 
-    <header class="header-page">
+        <header class="header-page">
 
-        <div class="header-page__bg"></div>
-    
-        <div class="wrapper-content flex-center">
+            <div class="header-page__bg"></div>
 
-            <div class="header-page__btn-box">
-                <button class="header-page__btn header-page__btn--menu">Menu</button>
+            <div class="wrapper-content flex-center">
+
+                <div class="header-page__btn-box">
+                    <button class="header-page__btn header-page__btn--menu">Menu</button>
+                </div>
+
+                <div class="logo-site">
+                    <div class="logo-site__armillaire"></div>
+                </div>
+
+                <div class="header-page__btn-box">
+                    <button class="header-page__btn header-page__btn--search">Rechercher</button>
+                    <button class="header-page__btn header-page__btn--metadata">Métadonnées</button>
+                </div>
+
             </div>
-            
-            <div class="logo-site">
-                <div class="logo-site__armillaire"></div>
-            </div>
 
-            <div class="header-page__btn-box">
-                <button class="header-page__btn header-page__btn--search">Rechercher</button>
-                <button class="header-page__btn header-page__btn--metadata">Métadonnées</button>
-            </div>
-        
+        </header>
+
+
+        <div class="wrapper-content">
+
+            <main class="main-page">
+
+                <div class="main-page__bg"></div>
+
+                <section class="content-page">
+
+                    <h1>Titre</h1>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit explicabo laboriosam
+                        exercitationem dicta, nam fugiat quae debitis soluta minus quidem provident ex enim, corrupti
+                        voluptas laudantium iure reprehenderit vitae, neque deleniti blanditiis iste repellat nihil! Sit
+                        magni distinctio ut eveniet, eos ea hic. Cum sit reprehenderit, neque laborum repellendus
+                        laboriosam numquam iure molestias atque pariatur fuga at explicabo assumenda possimus, ducimus
+                        aspernatur. Fugiat sint minus aliquid, repellat accusantium rerum exercitationem, fugit placeat
+                        ullam quod deleniti repellendus necessitatibus animi eveniet odio cumque quis. Delectus
+                        architecto ab corrupti quisquam fuga quo ipsa sequi aperiam nam dolorum atque, quas, doloremque
+                        itaque nulla? Consequuntur.</p>
+
+                    <h2>Lorem, ipsum.</h2>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ipsum reprehenderit
+                        architecto ea, a exercitationem voluptatibus nemo ut ullam non quibusdam, explicabo fugit
+                        obcaecati odit voluptatum error quidem harum. Veritatis laudantium aspernatur beatae, aliquid
+                        dolorum incidunt neque modi! Nihil obcaecati exercitationem similique vero eaque veritatis
+                        laborum quidem eligendi enim mollitia.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ipsum reprehenderit
+                        architecto ea, a exercitationem voluptatibus nemo ut ullam non quibusdam, explicabo fugit
+                        obcaecati odit voluptatum error quidem harum. Veritatis laudantium aspernatur beatae, aliquid
+                        dolorum incidunt neque modi! Nihil obcaecati exercitationem similique vero eaque veritatis
+                        laborum quidem eligendi enim mollitia.</p>
+
+                    <h2>Lorem, ipsum.</h2>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ipsum reprehenderit
+                        architecto ea, a exercitationem voluptatibus nemo ut ullam non quibusdam, explicabo fugit
+                        obcaecati odit voluptatum error quidem harum. Veritatis laudantium aspernatur beatae, aliquid
+                        dolorum incidunt neque modi! Nihil obcaecati exercitationem similique vero eaque veritatis
+                        laborum quidem eligendi enim mollitia.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ipsum reprehenderit
+                        architecto ea, a exercitationem voluptatibus nemo ut ullam non quibusdam, explicabo fugit
+                        obcaecati odit voluptatum error quidem harum. Veritatis laudantium aspernatur beatae, aliquid
+                        dolorum incidunt neque modi! Nihil obcaecati exercitationem similique vero eaque veritatis
+                        laborum quidem eligendi enim mollitia.</p>
+                </section>
+
+
+                <section class="lateral-page">
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iusto, minima asperiores maiores id
+                        laudantium vel ex dolore sequi ipsa?</p>
+
+                </section>
+
+            </main>
+
         </div>
-        
-    </header>
-    
-    
-    <div class="wrapper-content">
 
-        <main class="main-page">
 
-            <div class="main-page__bg"></div>
-
-            <section class="content-page">
-
-                <h1>Titre</h1>
-            
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit explicabo laboriosam exercitationem dicta, nam fugiat quae debitis soluta minus quidem provident ex enim, corrupti voluptas laudantium iure reprehenderit vitae, neque deleniti blanditiis iste repellat nihil! Sit magni distinctio ut eveniet, eos ea hic. Cum sit reprehenderit, neque laborum repellendus laboriosam numquam iure molestias atque pariatur fuga at explicabo assumenda possimus, ducimus aspernatur. Fugiat sint minus aliquid, repellat accusantium rerum exercitationem, fugit placeat ullam quod deleniti repellendus necessitatibus animi eveniet odio cumque quis. Delectus architecto ab corrupti quisquam fuga quo ipsa sequi aperiam nam dolorum atque, quas, doloremque itaque nulla? Consequuntur.</p>
-
-                <h2>Lorem, ipsum.</h2>
-
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ipsum reprehenderit architecto ea, a exercitationem voluptatibus nemo ut ullam non quibusdam, explicabo fugit obcaecati odit voluptatum error quidem harum. Veritatis laudantium aspernatur beatae, aliquid dolorum incidunt neque modi! Nihil obcaecati exercitationem similique vero eaque veritatis laborum quidem eligendi enim mollitia.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ipsum reprehenderit architecto ea, a exercitationem voluptatibus nemo ut ullam non quibusdam, explicabo fugit obcaecati odit voluptatum error quidem harum. Veritatis laudantium aspernatur beatae, aliquid dolorum incidunt neque modi! Nihil obcaecati exercitationem similique vero eaque veritatis laborum quidem eligendi enim mollitia.</p>
-                
-                <h2>Lorem, ipsum.</h2>
-                
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ipsum reprehenderit architecto ea, a exercitationem voluptatibus nemo ut ullam non quibusdam, explicabo fugit obcaecati odit voluptatum error quidem harum. Veritatis laudantium aspernatur beatae, aliquid dolorum incidunt neque modi! Nihil obcaecati exercitationem similique vero eaque veritatis laborum quidem eligendi enim mollitia.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ipsum reprehenderit architecto ea, a exercitationem voluptatibus nemo ut ullam non quibusdam, explicabo fugit obcaecati odit voluptatum error quidem harum. Veritatis laudantium aspernatur beatae, aliquid dolorum incidunt neque modi! Nihil obcaecati exercitationem similique vero eaque veritatis laborum quidem eligendi enim mollitia.</p>
-            </section>
-    
-    
-            <section class="lateral-page">
-
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam iusto, minima asperiores maiores id laudantium vel ex dolore sequi ipsa?</p>
-                
-            </section>
-
-        </main>
-        
     </div>
-    
 
-</div>
-
+    <script src="./assets/main.js"></script>
 
 </body>
 
