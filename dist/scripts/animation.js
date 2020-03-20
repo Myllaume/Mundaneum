c94f9ee0f4141cf95fb63ed7ed6f8916893d1a0b
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-03-20 09:47:12 +0100

    better roulement, responsive

--- dist/scripts/animation.js
+++ dist/scripts/animation.js
@@ -1,13 +1,13 @@
-function animLoadPage(fx) {
+function animLoadPage() {
     const conteneur = document.querySelector('.main-page');
     const rotateLogo = document.querySelector('.logo-site__bg');
     conteneur.classList.add('main-page--hidden');
     rotateLogo.classList.add('rotate-animation');
+    document.body.style.overflow = 'hidden';
 
     setTimeout(function() {
         conteneur.classList.remove('main-page--hidden');
         rotateLogo.classList.remove('rotate-animation');
-
-        fx();
+        document.body.style.overflow = 'auto';
     }, 5000);
 }
